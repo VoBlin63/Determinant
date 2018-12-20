7107583d8a52afa2c83ed2e0e633b6a733152111
Renat Buryachenko <renat.buryachenko@gmail.com>
2018-12-20 15:38:02 +0400

    Initial commit

---
+++ app/src/main/java/ru/buryachenko/determinant/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.buryachenko.determinant
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
