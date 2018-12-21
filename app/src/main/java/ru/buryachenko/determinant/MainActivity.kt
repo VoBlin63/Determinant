6645e81fcc2e7714f79a0fe7a94af55ac13f24d4
Renat Buryachenko <renat.buryachenko@gmail.com>
2018-12-21 18:21:00 +0400

    еще чуть-чуть

--- app/src/main/java/ru/buryachenko/determinant/MainActivity.kt
+++ app/src/main/java/ru/buryachenko/determinant/MainActivity.kt
@@ -1,8 +1,14 @@
 package ru.buryachenko.determinant
 
-import android.support.v7.app.AppCompatActivity
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.Constraints
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import ru.buryachenko.model.AtomMatrix
+import ru.buryachenko.viewmodel.DeterminantViewModel
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +16,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val model = AtomMatrix()
-        model.printOut()
+        val viewModel = ViewModelProviders.of(this).get(DeterminantViewModel::class.java)
+
+        val mainview = findViewById<ConstraintLayout>(R.id.main)
+
+//        val wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
+//
+//        val lParams = LinearLayout.LayoutParams(
+//            wrapContent, wrapContent
+//        )
+
+        val cParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT)
+
+        cParams.startToStart = ConstraintLayout.LayoutParams.START
+        val btn = Button(this)
+        btn.text = "TEST"
+        mainview.addView(btn)
+
+        viewModel.field.printOut()
     }
 }
--- app/src/main/java/ru/buryachenko/viewmodel/DeterminantViewModel.kt
+++ app/src/main/java/ru/buryachenko/viewmodel/DeterminantViewModel.kt
@@ -1,8 +1,13 @@
 package ru.buryachenko.viewmodel
 
+import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.ViewModel
+import ru.buryachenko.Determinant
+import ru.buryachenko.model.AtomMatrix
 
 class DeterminantViewModel : ViewModel() {
+
+    val field = AtomMatrix()
 
     fun init() {
 
