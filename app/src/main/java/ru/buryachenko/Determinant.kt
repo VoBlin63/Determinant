07ef4810471842d4a4af97afc976d9940e17bd28
VoBlin63 <voblin63@gmail.com>
2018-12-22 22:39:08 +0300

    пока уродливо, но появилось поле

--- app/src/main/java/ru/buryachenko/Determinant.kt
+++ app/src/main/java/ru/buryachenko/Determinant.kt
@@ -3,6 +3,7 @@ package ru.buryachenko
 import android.app.Application
 
 const val SIZE = 9
+const val LOGTAG = "DTMR"
 
 class Determinant : Application() {
 
--- app/src/main/java/ru/buryachenko/determinant/MainActivity.kt
+++ app/src/main/java/ru/buryachenko/determinant/MainActivity.kt
@@ -1,13 +1,16 @@
 package ru.buryachenko.determinant
 
 import android.arch.lifecycle.ViewModelProviders
+import android.graphics.Color
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
-import android.support.constraint.Constraints
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import ru.buryachenko.model.AtomMatrix
 import ru.buryachenko.viewmodel.DeterminantViewModel
+import android.support.constraint.ConstraintSet
+import android.util.Log
+import android.widget.TextView
+import ru.buryachenko.LOGTAG
+import ru.buryachenko.SIZE
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,21 +21,41 @@ class MainActivity : AppCompatActivity() {
 
         val viewModel = ViewModelProviders.of(this).get(DeterminantViewModel::class.java)
 
-        val mainview = findViewById<ConstraintLayout>(R.id.main)
+        val mainView = findViewById<ConstraintLayout>(R.id.main)
 
-//        val wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
-//
-//        val lParams = LinearLayout.LayoutParams(
-//            wrapContent, wrapContent
-//        )
+        val set = ConstraintSet()
+        for (row in 1..SIZE) {
+            var benchMark = ConstraintSet.PARENT_ID
+            for (col in 1..SIZE) {
+                val picture = TextView(this)
+                picture.id = 100 + row * SIZE + col
 
-        val cParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT)
+                picture.width = resources.getDimensionPixelSize(R.dimen.cellWidth)
+                picture.height = resources.getDimensionPixelSize(R.dimen.cellHeight)
+                picture.textSize = 15F
+                picture.letterSpacing = 0.11F
+                picture.text = "123456789"
+                picture.setLines(3)
+                picture.setBackgroundColor(resources.getColor(if ((row + col) % 2 == 0) R.color.colorBackgroundFirst else R.color.colorBackgroundSecond))
+                mainView.addView(picture)
+                set.clone(mainView)
+                set.clear(picture.id, ConstraintSet.TOP)
+                set.clear(picture.id, ConstraintSet.LEFT)
+                set.connect(picture.id, ConstraintSet.TOP, benchMark, ConstraintSet.TOP, 0)
+                if (benchMark == ConstraintSet.PARENT_ID) {
+                    set.connect(picture.id, ConstraintSet.TOP, benchMark, ConstraintSet.TOP, (row - 1) * resources.getDimensionPixelSize(R.dimen.cellHeight))
+                }
+                else {
+                    set.connect(picture.id, ConstraintSet.TOP, benchMark, ConstraintSet.TOP, 0)
+                    set.connect(picture.id, ConstraintSet.LEFT, benchMark, ConstraintSet.RIGHT, 0)
+                }
+                set.applyTo(mainView)
+                benchMark = picture.id
+            }
 
-        cParams.startToStart = ConstraintLayout.LayoutParams.START
-        val btn = Button(this)
-        btn.text = "TEST"
-        mainview.addView(btn)
+        }
 
-        viewModel.field.printOut()
+
+//        viewModel.field.printOut()
     }
 }
